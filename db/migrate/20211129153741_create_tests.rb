118c9eecc9f41f213f59fc9e06161b4b31789d27
sleepingming <daniltop1@bk.ru>
2021-11-30 15:11:42 +0300

    fixed models and migrations

--- db/migrate/20211129153741_create_tests.rb
+++ db/migrate/20211129153741_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.string :title, null: false
       t.integer :level, null: false, default: 0
       t.references :category, foreign_key: true, null: false
-      t.references :user, foreign_key: true, null: false
+      t.references :author, foreign_key: { to_table: :users }, null: false
       t.timestamps
     end
   end
