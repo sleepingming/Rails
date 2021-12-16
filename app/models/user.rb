6c638b3d7e6513c71642856ed0e716ca768141a0
sleepingming <daniltop1@bk.ru>
2021-12-16 17:00:39 +0300

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
 
-  has_many: created_tests, class_name: "Test", foreign_key: 'author_id', dependent: :destroy
-  has_many: tests, through: results, dependent: : destroy
-  has_many: results, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
+  has_many :tests, through: :results, dependent: :destroy
+  has_many :results, dependent: :destroy
 
-  validates:email, :password, presence: true
+  validates :email, :password, presence: true
 
   def passed_tests(level)
     Test
-    .joins(:results)
-    .where(level: level, results: { user: id })
+      .joins(:results)
+      .where(level: level, results: { user: id })
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,34 +11,34 @@ users = User.create!([
   ])
 
 categories = Category.create!([
-  {title: "frontend"}
+  {title: "frontend"},
   {title: "backend"}
   ])
 
 tests = Test.create!([
-  {title: "Rails", level: 1, category: categories[0], author: users[0]}
+  {title: "Rails", level: 1, category: categories[0], author: users[0]},
   {title: "HTML", level: 0, category: categories[0], author: users[0]}
   ])
 
 questions = Question.create!([
-  {title: "В каком году был создан Rails?", test: tests[0]}
-  {title: "Кто является автором Rails?", test: tests[0]}
-  {title: "В каком году был разработан HTML?", test: tests[1]}
+  {title: "В каком году был создан Rails?", test: tests[0]},
+  {title: "Кто является автором Rails?", test: tests[0]},
+  {title: "В каком году был разработан HTML?", test: tests[1]},
   {title: "Кто является разработчиком HTML?", test: tests[1]}
   ])
 
 answers = Answer.create!([
-  {title: "2005", correct: 1, question: questions[0]}
-  {title: "2001", correct: 0, question: questions[0]}
-  {title: "Дэвид Ханссон", correct: 1, question: questions[1]}
-  {title: "Тим Бернерс-Ли", correct: 0, question: questions[1]}
-  {title: "1993", correct: 1, question: questions[2]}
-  {title: "1996", correct: 0, question: questions[2]}
-  {title: "Тим Бернерс-Ли", correct: 1, question: questions[3]}
+  {title: "2005", correct: 1, question: questions[0]},
+  {title: "2001", correct: 0, question: questions[0]},
+  {title: "Дэвид Ханссон", correct: 1, question: questions[1]},
+  {title: "Тим Бернерс-Ли", correct: 0, question: questions[1]},
+  {title: "1993", correct: 1, question: questions[2]},
+  {title: "1996", correct: 0, question: questions[2]},
+  {title: "Тим Бернерс-Ли", correct: 1, question: questions[3]},
   {title: "Дэвид Ханссон", correct: 0, question: questions[3]}
   ])
 
 results = Result.create!([
-  {user: users[0], test: tests[0]}
+  {user: users[0], test: tests[0]},
   {user: users[0], test: tests[1]}
   ])
