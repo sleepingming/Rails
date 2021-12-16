41c3a2a3b2e2007d12b4d816d313370a0b56e44b
sleepingming <daniltop1@bk.ru>
2021-12-16 22:03:14 +0300

    app logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -41,10 +41,11 @@ class QuestionsController < ApplicationController
   def find_test
     @test = Test.find(params[:test_id])
   end
-
+  
   def find_question
     @question = Question.find(params[:id])
   end
+
 
   def rescue_with_question_not_found
     render plain: 'Вопрос не найден'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ApplicationRecord
 
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
-  has_many :tests, through: :results, dependent: :destroy
+  has_many :tests, through: :test_passages, dependent: :destroy
   has_many :results, dependent: :destroy
+  has_many :test_passages
 
   validates :email, :password, presence: true
 
   def passed_tests(level)
     Test
-      .joins(:results)
-      .where(level: level, results: { user: id })
+      .joins(:test_passages)
+      .where(level: level, test_passages: { user: id })
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+
   end
 end
