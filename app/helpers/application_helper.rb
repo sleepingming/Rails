8b42bb587dd0abadbd779efb050ebd1bd6f956d8
sleepingming <81436295+sleepingming@users.noreply.github.com>
2021-12-29 18:21:34 +0300

    helper fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
 
   def flash_message(type)
     if flash[type]
-      content_tag :p, flash[type], class: 'flash alert'
+      content_tag :p, flash[type], class: 'flash #{type}'
     end
   end
 end
