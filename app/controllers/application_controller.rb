42eafda915e4c8258181b228b6bac424bca39846
sleepingming <daniltop1@bk.ru>
2022-01-09 18:41:07 +0300

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,9 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  helper_method :current_user,
-                :logged_in?
-
-  private
-
-  def authenticate_user!
-    unless current_user
-      redirect_to login_path, alert: 'Неверный логин или пароль'
-      cookies[:previous_url] = request.original_url
+  def redirect_after_sign_in(user)
+    if user.is_a?(Admin)
+      admin_tests_path
     end
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
   end
 end
