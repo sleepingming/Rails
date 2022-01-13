18777600e6a925bfc358f6e2e1568d3c01781265
sleepingming <daniltop1@bk.ru>
2022-01-13 18:40:07 +0300

    fixed method admin? and controllers

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,10 @@
 class Admin::BaseController < ApplicationController
-
   before_action :authenticate_user!
   before_action :admin_required!
 
   private
 
   def admin_required!
-    redirect_to root_path, alert: 'Вы не можете просмотреть эту страницу' unless current_user.admin?(current_user)
+    redirect_to root_path, alert: 'Вы не можете просмотреть эту страницу' unless current_user.admin?
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(user)
-    if user.admin?(user)
+    if user.admin?
       admin_tests_path
     else
       super
