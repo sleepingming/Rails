1991318e76d4f737d350e57e83dcd22aa1fd359d
sleepingming <daniltop1@bk.ru>
2022-02-28 16:53:18 +0300

    fixed

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,11 +2,6 @@ class Test < ApplicationRecord
   scope :easy_level, -> { where(level: 0..1) }
   scope :medium_level, -> { where(level: 2..4) }
   scope :hard_level, -> { where(level: 5..Float::INFINITY) }
-  scope :by_category, (lambda do |category_name|
-                         joins(:category)
-                           .where(categories: { title: category_name })
-                           .order(title: :desc)
-                         end)
 
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
